e184116117f24fe7e06af02858e34fa57398f9e4
crmgunter <crmgunter@gmail.com>
2018-03-29 15:06:32 -0400

    set up components for react

---
+++ client/src/components/ArtistListView.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ArtistListView extends Component {
+    render() {
+        return (
+            <div>
+                <video class="video-container video-container-overlay" autoplay="" loop="" muted="" data-reactid=".0.1.0.0">
+            <source type="video/mp4" data-reactid=".0.1.0.0.0" src="https://i.imgur.com/ewMMHdl.mp4" />>
+            </video>
+            </div>
+        );
+    }
+}
+
+export default ArtistListView;
---
+++ client/src/components/SingleArtistView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SingleArtistView extends Component {
+    render() {
+        return (
+            <div>
+                <img src="https://i.imgur.com/QsfneFh.gifv" alt="bald" />
+            </div>
+        );
+    }
+}
+
+export default SingleArtistView;
